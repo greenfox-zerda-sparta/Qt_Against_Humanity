a4f4b5729f2a1bc80647c1d9d3139f9c371ae224
Jávorszky Mazsi <javorszky.mariann@gmail.com>
2017-01-26 11:23:52 +0100

    kiscicák

--- cah/cah/main.cpp
+++ cah/cah/main.cpp
@@ -1,10 +1,12 @@
 #include "cah.h"
 #include <QtWidgets/QApplication>
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     cah w;
     w.show();
+    std::cout << "kiscicák";
     return a.exec();
 }
