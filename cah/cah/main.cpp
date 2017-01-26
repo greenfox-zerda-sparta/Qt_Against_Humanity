a8b07561fec6e779a8421e85d376154cb914bb0b
Julia Baki <bakijulia@gmail.com>
2017-01-26 17:30:56 +0100

    client

--- cah/cah/main.cpp
+++ cah/cah/main.cpp
@@ -3,17 +3,18 @@
 #include <QtWidgets/QApplication>
 #include <iostream>
 #include "Client.h"
+#include <gamegui.h>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    cah w;
-    w.show();
-<<<<<<< HEAD
+    //cah w;
+    //w.show();
+   // GameGui gamegui;
+    //gamegui.show();
     Client client;
-    std::cout << "kiscicák";
-=======
-    GameLogic gamelogic;
->>>>>>> master
+    //std::cout << "kiscicák";
+   // GameLogic gamelogic;
+    
     return a.exec();
 }
